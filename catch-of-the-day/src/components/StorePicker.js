8f2d79ab793a76c880b61403f9b0df18a862cecf
penchochris <garabchris@gmail.com>
2017-12-08 02:08:46 +0100

    Making objects

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,8 +4,10 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault();
-    console.log('You Changed the URL');
-    console.log(this.storeInput.value);
+
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    this.context.router.transitionTo(`/store/${storeId}`)
   }
 
   render() {
@@ -17,6 +19,10 @@ class StorePicker extends React.Component {
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
