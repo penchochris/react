f737e3741afa137c62bc1e0890726a3058413585
penchochris <garabchris@gmail.com>
2017-12-10 03:49:16 +0100

    AddingFirebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyClMVrerxV-GnOw7VZ5_Q6LMqvG8kOwZi0",
+    authDomain: "catch-of-the-day-pencho.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-pencho.firebaseio.com"
+});
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,26 +4,61 @@ import Order from './Order';
 import Inventory from './Inventory';
 import Fish from './Fish';
 
+import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
-  
-  constructor(){
+  constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
-    
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+
     this.state = {
       fishes: {},
       order: {}
-    }
+    };
+  }
+
+  //Estas funciones son propias de react
+  //Component Specs
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+    , {
+        context: this,
+        state: 'fishes'
+      });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  
+
+  componentWillUpdate(nextProps, nextState) {
+    this.ref = base.syncState(``);
   }
 
   addFish(fish) {
     const fishes = {...this.state.fishes};
     const timestamp = Date.now();
-
     fishes[`fish-${timestamp}`] = fish;
     this.setState({ fishes });
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
+  addToOrder(key) {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
   }
 
   render() {
@@ -33,14 +68,14 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
           <ul className="list-of-fishes">
             {
-              Object.keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this
-                  .state.fishes[key]}/>)
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
-        <Order />
-        <Inventory addFish={this.addFish}/>
+        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
